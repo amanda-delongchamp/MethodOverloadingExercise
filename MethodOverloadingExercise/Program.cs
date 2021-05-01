47ce1d0c7e2f0ca7c3c0f94a6ad3307c7ff11483
amanda-delongchamp <amanda.delongchamp@gmail.com>
2021-05-01 12:07:10 -0700

    Add project files.

---
+++ MethodOverloadingExercise/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MethodOverloadingExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer1 = Add(2, 2);
+            var answer2 = Add(2444m, .23533m);
+            var answer3 = Add(10, 100, true);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool maybe)
+        {
+            int total = num1 + num2;
+
+            if (maybe == true)
+            {
+                if (total == 1)
+                {
+                    return $"{total} dollar";
+                }
+                else
+                {
+                    return $"{total} dollars";
+                }
+            }
+            else
+            {
+                return $"{total}";
+            }
+        }
+    }
+}
